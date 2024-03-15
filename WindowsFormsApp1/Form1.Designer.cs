f370cd9da1ae5df8d0991cceeb9cda8c5d757443
adinarus <125950892+adinarus@users.noreply.github.com>
2024-03-15 11:56:18 +0200

    Refactor code and added image filtering

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,18 +33,26 @@
             this.button_histogram = new System.Windows.Forms.Button();
             this.numericUpDownAlpha = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownBeta = new System.Windows.Forms.NumericUpDown();
-            this.button_image_modify = new System.Windows.Forms.Button();
+            this.button_contrast_brightness = new System.Windows.Forms.Button();
+            this.button_gamma = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.numericUpDownGamma = new System.Windows.Forms.NumericUpDown();
+            this.buttonFiltering = new System.Windows.Forms.Button();
+            this.checkBoxRed = new System.Windows.Forms.CheckBox();
+            this.checkBoxGreen = new System.Windows.Forms.CheckBox();
+            this.checkBoxBlue = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlpha)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBeta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGamma)).BeginInit();
             this.SuspendLayout();
             // 
             // button_image_upload
             // 
             this.button_image_upload.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button_image_upload.Location = new System.Drawing.Point(120, 60);
+            this.button_image_upload.Location = new System.Drawing.Point(32, 58);
             this.button_image_upload.Name = "button_image_upload";
-            this.button_image_upload.Size = new System.Drawing.Size(131, 36);
+            this.button_image_upload.Size = new System.Drawing.Size(147, 36);
             this.button_image_upload.TabIndex = 0;
             this.button_image_upload.Text = "Upload image";
             this.button_image_upload.UseVisualStyleBackColor = false;
@@ -52,18 +60,18 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(571, 46);
+            this.pictureBox1.Location = new System.Drawing.Point(446, 58);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(431, 292);
+            this.pictureBox1.Size = new System.Drawing.Size(654, 388);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // button_histogram
             // 
-            this.button_histogram.Location = new System.Drawing.Point(108, 137);
+            this.button_histogram.Location = new System.Drawing.Point(32, 120);
             this.button_histogram.Name = "button_histogram";
-            this.button_histogram.Size = new System.Drawing.Size(143, 36);
+            this.button_histogram.Size = new System.Drawing.Size(147, 36);
             this.button_histogram.TabIndex = 2;
             this.button_histogram.Text = "Create histogram";
             this.button_histogram.UseVisualStyleBackColor = true;
@@ -77,7 +85,7 @@
             0,
             0,
             65536});
-            this.numericUpDownAlpha.Location = new System.Drawing.Point(161, 223);
+            this.numericUpDownAlpha.Location = new System.Drawing.Point(186, 194);
             this.numericUpDownAlpha.Maximum = new decimal(new int[] {
             3,
             0,
@@ -96,31 +104,120 @@
             0,
             0,
             0});
-            this.numericUpDownAlpha.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // numericUpDownBeta
             // 
-            this.numericUpDownBeta.Location = new System.Drawing.Point(161, 306);
+            this.numericUpDownBeta.Location = new System.Drawing.Point(185, 222);
             this.numericUpDownBeta.Name = "numericUpDownBeta";
             this.numericUpDownBeta.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownBeta.TabIndex = 4;
             // 
-            // button_image_modify
+            // button_contrast_brightness
             // 
-            this.button_image_modify.Location = new System.Drawing.Point(305, 250);
-            this.button_image_modify.Name = "button_image_modify";
-            this.button_image_modify.Size = new System.Drawing.Size(122, 51);
-            this.button_image_modify.TabIndex = 5;
-            this.button_image_modify.Text = "Modify image";
-            this.button_image_modify.UseVisualStyleBackColor = true;
-            this.button_image_modify.Click += new System.EventHandler(this.button_image_modify_Click);
+            this.button_contrast_brightness.Location = new System.Drawing.Point(32, 194);
+            this.button_contrast_brightness.Name = "button_contrast_brightness";
+            this.button_contrast_brightness.Size = new System.Drawing.Size(147, 51);
+            this.button_contrast_brightness.TabIndex = 5;
+            this.button_contrast_brightness.Text = "Contrast/Brightness";
+            this.button_contrast_brightness.UseVisualStyleBackColor = true;
+            this.button_contrast_brightness.Click += new System.EventHandler(this.button_contrast_brightness_Click);
+            // 
+            // button_gamma
+            // 
+            this.button_gamma.Location = new System.Drawing.Point(32, 284);
+            this.button_gamma.Name = "button_gamma";
+            this.button_gamma.Size = new System.Drawing.Size(147, 35);
+            this.button_gamma.TabIndex = 6;
+            this.button_gamma.Text = "button_Gamma";
+            this.button_gamma.UseVisualStyleBackColor = true;
+            this.button_gamma.Click += new System.EventHandler(this.button_gamma_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(200, 58);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(121, 36);
+            this.buttonReset.TabIndex = 8;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // numericUpDownGamma
+            // 
+            this.numericUpDownGamma.DecimalPlaces = 2;
+            this.numericUpDownGamma.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownGamma.Location = new System.Drawing.Point(186, 291);
+            this.numericUpDownGamma.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownGamma.Name = "numericUpDownGamma";
+            this.numericUpDownGamma.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownGamma.TabIndex = 9;
+            this.numericUpDownGamma.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonFiltering
+            // 
+            this.buttonFiltering.Location = new System.Drawing.Point(22, 340);
+            this.buttonFiltering.Name = "buttonFiltering";
+            this.buttonFiltering.Size = new System.Drawing.Size(71, 36);
+            this.buttonFiltering.TabIndex = 10;
+            this.buttonFiltering.Text = "Filter";
+            this.buttonFiltering.UseVisualStyleBackColor = true;
+            this.buttonFiltering.Click += new System.EventHandler(this.buttonFiltering_Click);
+            // 
+            // checkBoxRed
+            // 
+            this.checkBoxRed.AutoSize = true;
+            this.checkBoxRed.Location = new System.Drawing.Point(22, 382);
+            this.checkBoxRed.Name = "checkBoxRed";
+            this.checkBoxRed.Size = new System.Drawing.Size(55, 20);
+            this.checkBoxRed.TabIndex = 14;
+            this.checkBoxRed.Text = "Red";
+            this.checkBoxRed.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGreen
+            // 
+            this.checkBoxGreen.AutoSize = true;
+            this.checkBoxGreen.Location = new System.Drawing.Point(22, 408);
+            this.checkBoxGreen.Name = "checkBoxGreen";
+            this.checkBoxGreen.Size = new System.Drawing.Size(66, 20);
+            this.checkBoxGreen.TabIndex = 15;
+            this.checkBoxGreen.Text = "Green";
+            this.checkBoxGreen.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBlue
+            // 
+            this.checkBoxBlue.AutoSize = true;
+            this.checkBoxBlue.Location = new System.Drawing.Point(21, 434);
+            this.checkBoxBlue.Name = "checkBoxBlue";
+            this.checkBoxBlue.Size = new System.Drawing.Size(56, 20);
+            this.checkBoxBlue.TabIndex = 16;
+            this.checkBoxBlue.Text = "Blue";
+            this.checkBoxBlue.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 450);
-            this.Controls.Add(this.button_image_modify);
+            this.ClientSize = new System.Drawing.Size(1132, 526);
+            this.Controls.Add(this.checkBoxBlue);
+            this.Controls.Add(this.checkBoxGreen);
+            this.Controls.Add(this.checkBoxRed);
+            this.Controls.Add(this.buttonFiltering);
+            this.Controls.Add(this.numericUpDownGamma);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.button_gamma);
+            this.Controls.Add(this.button_contrast_brightness);
             this.Controls.Add(this.numericUpDownBeta);
             this.Controls.Add(this.numericUpDownAlpha);
             this.Controls.Add(this.button_histogram);
@@ -132,7 +229,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlpha)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBeta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGamma)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -143,7 +242,14 @@
         private System.Windows.Forms.Button button_histogram;
         private System.Windows.Forms.NumericUpDown numericUpDownAlpha;
         private System.Windows.Forms.NumericUpDown numericUpDownBeta;
-        private System.Windows.Forms.Button button_image_modify;
+        private System.Windows.Forms.Button button_contrast_brightness;
+        private System.Windows.Forms.Button button_gamma;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.NumericUpDown numericUpDownGamma;
+        private System.Windows.Forms.Button buttonFiltering;
+        private System.Windows.Forms.CheckBox checkBoxRed;
+        private System.Windows.Forms.CheckBox checkBoxGreen;
+        private System.Windows.Forms.CheckBox checkBoxBlue;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,31 +10,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV.UI;
+using static System.Windows.Forms.AxHost;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        Image<Bgr, Byte> My_Image;
+        private ImageProcessor ImageProcessor;
+
         public Form1()
         {
             InitializeComponent();
+            ImageProcessor = new ImageProcessor();
         }
-
 
         private void button_image_upload_Click(object sender, EventArgs e)
         {
-            LoadImage();
-            
-        }
-
-        private void LoadImage()
-        {
-            OpenFileDialog Openfile = new OpenFileDialog();
-            if (Openfile.ShowDialog() == DialogResult.OK)
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
             {
-                My_Image = new Image<Bgr, byte>(Openfile.FileName);
-                pictureBox1.Image = My_Image.ToBitmap();
+                ImageProcessor.LoadImage(openFile.FileName);
+                pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
             }
         }
 
@@ -45,10 +41,10 @@ namespace WindowsFormsApp1
 
         private void ShowHistogram()
         {
-            if (My_Image != null)
+            if (ImageProcessor.ProcessedImage != null)
             {
                 HistogramViewer v = new HistogramViewer();
-                v.HistogramCtrl.GenerateHistograms(My_Image, 255);
+                v.HistogramCtrl.GenerateHistograms(ImageProcessor.ProcessedImage, 255);
                 v.Show();
             }
             else
@@ -57,22 +53,151 @@ namespace WindowsFormsApp1
             }
         }
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        private void button_gamma_Click(object sender, EventArgs e)
         {
-
+            double gamma = (double)numericUpDownGamma.Value;
+            ImageProcessor.ApplyGammaCorrection(gamma);
+            pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
         }
 
-        private void button_image_modify_Click(object sender, EventArgs e)
+        private void button_contrast_brightness_Click(object sender, EventArgs e)
         {
-            decimal alpha;
-            decimal beta;
-            alpha = numericUpDownAlpha.Value;
-            beta = numericUpDownBeta.Value;
+            double alpha = (double)numericUpDownAlpha.Value;
+            double beta = (double)numericUpDownBeta.Value;
+            ImageProcessor.ApplyContrastBrightness(alpha, beta);
+            pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
+        }
 
-            var outputImage = My_Image.Mul((float)alpha) + (float)beta;
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            ImageProcessor.ResetImage();
+            pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
+        }
 
-            pictureBox1.Image = outputImage.ToBitmap();
-             
+        private void buttonFiltering_Click(object sender, EventArgs e)
+        {
+
+
+            int r = 0, g = 0, b = 0;
+            if (checkBoxRed.Checked) {
+                r = 1;
+            }
+            if (checkBoxGreen.Checked)
+            {
+                g = 1;
+            }
+            if (checkBoxBlue.Checked)
+            {
+                b = 1;
+            }
+
+            ImageProcessor.ApplyFilter(r,g,b);
+           
+            pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
+        }
+
+        private void buttonFilterRed_Click(object sender, EventArgs e)
+        {
+            ImageProcessor.ApplyFilter(1, 0, 0);
+            pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
+        }
+
+        private void buttonFilterGreen_Click(object sender, EventArgs e)
+        {
+            ImageProcessor.ApplyFilter(0, 1, 0);
+            pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
+        }
+
+        private void buttonFilterBlue_Click(object sender, EventArgs e)
+        {
+            ImageProcessor.ApplyFilter(0, 0, 1);
+            pictureBox1.Image = ImageProcessor.ProcessedImage.ToBitmap();
+        }
+    }
+
+    public class ImageProcessor
+    {
+        private Image<Bgr, Byte> OriginalImage;
+
+        public Image<Bgr, Byte> ProcessedImage { get; private set; }
+
+        public void LoadImage(string filePath)
+        {
+            OriginalImage = new Image<Bgr, byte>(filePath);
+            ProcessedImage = OriginalImage.Clone();
+        }
+
+        public void ApplyGammaCorrection(double gamma)
+        {
+            if (OriginalImage != null)
+            {
+                ProcessedImage._GammaCorrect(gamma);
+            }
+        }
+
+        public void ApplyContrastBrightness(double alpha, double beta)
+        {
+            if (OriginalImage != null)
+            {
+                ProcessedImage = ProcessedImage.Mul(alpha) + beta;
+            }
+        }
+
+        public void ApplyRedFilter()
+        {
+            if (OriginalImage != null)
+            {
+      
+                Image<Bgr, Byte> outputImage = new Image<Bgr, byte>(OriginalImage.Size);
+                OriginalImage.CopyTo(outputImage);
+
+                var data = outputImage.Data;
+
+                for (int i = 0; i < outputImage.Width; i++)
+                {
+                    for (int j = 0; j < outputImage.Height; j++)
+                    {
+
+                        data[j, i, 0] = 0;
+                        data[j, i, 1] = 0;
+                     
+
+                    }
+                }
+               
+                ProcessedImage = outputImage;
+            }
+        }
+        public void ApplyFilter(int r, int g, int b)
+        {
+            if (OriginalImage != null)
+            {
+
+                Image<Bgr, Byte> outputImage = new Image<Bgr, byte>(OriginalImage.Size);
+                OriginalImage.CopyTo(outputImage);
+
+                var data = outputImage.Data;
+
+                for (int i = 0; i < outputImage.Width; i++)
+                {
+                    for (int j = 0; j < outputImage.Height; j++)
+                    {
+                        if (b == 0) data[j, i, 0] = 0;
+                        if (g == 0) data[j, i, 1] = 0;
+                        if (r == 0) data[j, i, 2] = 0;
+                    }
+                }
+
+                ProcessedImage = outputImage;
+            }
+
+        }
+        public void ResetImage()
+        {
+            if (OriginalImage != null)
+            {
+                ProcessedImage = OriginalImage.Clone();
+            }
         }
     }
 }
